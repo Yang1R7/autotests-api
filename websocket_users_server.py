a2c5f8a688de50011038da3c455294a4b995caa9
Max <maks.krutikov.2002@gmail.com>
2025-10-25 15:15:12 +0300

    Знакомство с WebSocket протоколом

--- websocket_users_server.py
+++ websocket_users_server.py
@@ -7,7 +7,7 @@ from websockets import ServerConnection
 
 async def echo(websockets: ServerConnection):
     async for message in websockets:
-        print(f"Получено сообщение {message}")
+        print(f"Получено сообщение от пользователя: {message}")
         response = f"Сервер получил {message}"
         for _ in range(5):
             await websockets.send(response)
