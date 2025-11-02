69846ff1ffa378adf13611b525ef9391afcb105b
Max <maks.krutikov.2002@gmail.com>
2025-11-03 02:54:59 +0400

    Практикуемся в написании API автотестов. Часть 1: Практика написания негативных API автотестов для работы с файлами

---
+++ clients/errors_schema.py
@@ -0,0 +1,32 @@
+from typing import Any, Annotated
+
+from pydantic import BaseModel, Field, ConfigDict
+
+
+
+class ValidationErrorSchema(BaseModel):
+    """
+     Модель, описывающая структуру ошибки валидации API.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    type: str
+    input: Any
+    context: Annotated[dict[str, Any], Field(alias="ctx")]
+    message: Annotated[str, Field(alias="msg")]
+    location: Annotated[list[str], Field(alias="loc")]
+
+
+class ValidationErrorResponseSchema(BaseModel):
+    """
+    Модель, описывающая структуру ответа API с ошибкой валидации.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    details: list[ValidationErrorSchema] = Field(alias="detail")
+
+
+class InternalErrorResponseSchema(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+    details: Annotated[str,Field(alias="detail")]
+
