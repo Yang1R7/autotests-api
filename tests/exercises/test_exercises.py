6cfbbd5d924e915074fd67765936e207c2c295e9
Max <maks.krutikov.2002@gmail.com>
2025-11-04 20:17:07 +0400

    Практическая работа с Allure аннотациями

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -1,6 +1,9 @@
 from http import HTTPStatus
 
+import allure
 import pytest
+from allure_commons.types import Severity
+from pygments.lexers.scripting import all_lua_builtins
 
 from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_client import ExercisesClient
@@ -9,6 +12,10 @@ from clients.exercises.exercises_schema import CreateExerciseRequestSchema, Crea
     GetExercisesResponseSchema
 from fixtures.courses import CoursesFixture
 from fixtures.exercises import ExerciseFixture
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
     assert_update_exercise_response, assert_exercise_not_found_response, assert_get_exercises_response
@@ -17,7 +24,17 @@ from tools.assertions.schema import validate_json_schema
 
 @pytest.mark.exercises
 @pytest.mark.regression
+@allure.tag(AllureTag.EXERCISES, AllureTag.REGRESSION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.EXERCISES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.EXERCISES)
 class TestExercises:
+    @allure.tag(AllureTag.CREATE_ENTITY)
+    @allure.story(AllureStory.CREATE_ENTITY)
+    @allure.sub_suite(AllureStory.CREATE_ENTITY)
+    @allure.title("Create exercise")
+    @allure.severity(Severity.BLOCKER)
     def test_create_exercise(
             self,
             exercises_client: ExercisesClient,
@@ -32,6 +49,11 @@ class TestExercises:
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_create_exercise_response(actual=response_data, expected=request)
 
+    @allure.tag(AllureTag.GET_ENTITY)
+    @allure.story(AllureStory.GET_ENTITY)
+    @allure.sub_suite(AllureStory.GET_ENTITY)
+    @allure.title("Get exercise")
+    @allure.severity(Severity.BLOCKER)
     def test_get_exercise(
             self,
             exercises_client: ExercisesClient,
@@ -47,6 +69,11 @@ class TestExercises:
 
         validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
 
+    @allure.tag(AllureTag.UPDATE_ENTITY)
+    @allure.story(AllureStory.UPDATE_ENTITY)
+    @allure.sub_suite(AllureStory.UPDATE_ENTITY)
+    @allure.title("Update exercise")
+    @allure.severity(Severity.CRITICAL)
     def test_update_exercise(
             self,
             exercises_client: ExercisesClient,
@@ -63,6 +90,11 @@ class TestExercises:
 
         validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
 
+    @allure.tag(AllureTag.DELETE_ENTITY)
+    @allure.story(AllureStory.DELETE_ENTITY)
+    @allure.sub_suite(AllureStory.DELETE_ENTITY)
+    @allure.title("Delete exercise")
+    @allure.severity(Severity.CRITICAL)
     def test_delete_exercise(
             self,
             exercises_client: ExercisesClient,
@@ -81,7 +113,13 @@ class TestExercises:
 
         validate_json_schema(instance=get_response.json(), schema=get_response_data.model_json_schema())
 
-    def test_get_exercises(self,exercises_client: ExercisesClient, function_exercise: ExerciseFixture, function_courses:CoursesFixture):
+    @allure.tag(AllureTag.GET_ENTITIES)
+    @allure.story(AllureStory.GET_ENTITIES)
+    @allure.sub_suite(AllureStory.GET_ENTITIES)
+    @allure.title("Get exercises")
+    @allure.severity(Severity.BLOCKER)
+    def test_get_exercises(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture,
+                           function_courses: CoursesFixture):
         query = GetExercisesQuerySchema(course_id=function_courses.response.course.id)
 
         response = exercises_client.get_exercises_api(query=query)
@@ -93,4 +131,4 @@ class TestExercises:
             get_exercises_response=response_data
         )
 
-        validate_json_schema(instance=response.json(),schema=response_data.model_json_schema())
+        validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
